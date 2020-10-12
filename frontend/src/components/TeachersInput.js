3c7a0a3e55c542d0871f33508dc6a50bea2f9d4e
Jeffrey Martinez <martinezjf2@gmail.com>
2020-10-12 13:09:52 -0400

    completed controlled form for teachers input

--- frontend/src/components/TeachersInput.js
+++ frontend/src/components/TeachersInput.js
@@ -2,23 +2,44 @@ import React, { Component } from "react";
 import {connect} from 'react-redux';
 
 class TeachersInput extends Component {
+    state ={
+        name: '',
+        degree: '',
+        phone_number: '',
+        email: ''
+    }
 
+
+    handleChange = (event) => {
+        this.setState ({
+            [event.target.name]: event.target.value,
+            [event.target.degree]: event.target.value,
+            [event.target.phone_number]: event.target.value,
+            [event.target.email]: event.target.value
+        })
+
+    }
+
+    handleSubmit = () => {
+
+    }
 
 render() {
 
     return (
 
         <div>
-           <form>
+           <form onSubmit={this.handleSubmit}>
                 <label>Name: </label>
-                <input type="text" /><br/><br/>
+                <input type="text" name="name" value={this.state.name} onChange={this.handleChange} /><br/><br/>
                 {/* can make degrees as options with what i have by iterating through */}
                 <label>Degree: </label>
-                <input type="text" /><br/><br/>
+                <input type="text" name="degree" value={this.state.degree} onChange={this.handleChange} /><br/><br/>
                 <label>Phone Number: </label>
-                <input type="text" /><br/><br/>
+                <input type="text" name="phone_number" value={this.state.phone_number} onChange={this.handleChange} /><br/><br/>
                 <label>Email: </label>
-                <input type="text" /><br/><br/>
+                <input type="text" name="email" value={this.state.email} onChange={this.handleChange}  /><br/><br/>
+                <input type="submit" /> <br/>
            </form>
            <br/>
            
